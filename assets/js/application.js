82673bd8faa2f1dfc6fe56dac853358f736e4a07
Yoni Touboul <yonitouboul@hotmail.com>
2020-07-15 13:32:23 +0200

    List, typed JS & fixes

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,5 @@
+
+
 const arrow = document.querySelector('#first-arrow');
     arrow.addEventListener('click', (e) => {
         e.preventDefault();
@@ -5,3 +7,10 @@ const arrow = document.querySelector('#first-arrow');
             behavior: 'smooth'
         });
     });
+
+let typed = new Typed('#typed', {
+    strings: ['MAMA'],
+    typeSpeed: 200,
+    showCursor: false
+  });
+
